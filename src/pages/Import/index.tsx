f44aa07bf51dcef605e2e3449137c80d7c386ceb
Lucas <lucas.raziel@gmail.com>
2020-05-13 23:13:07 -0300

    finish

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,33 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (uploadedFiles[0]) {
+      data.append('file', uploadedFiles[0].file);
+    } else {
+      return;
+    }
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      setUploadedFiles([]);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    if (files[0]) {
+      setUploadedFiles([
+        ...uploadedFiles,
+        {
+          file: files[0],
+          name: files[0].name,
+          readableSize: `${files[0].size}Kb`,
+        },
+      ]);
+    }
   }
 
   return (
